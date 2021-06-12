9468332b911f0d319cd1c546487bccef4bad0e6b
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-06-12 23:29:01 +0200

    Minor amendment

--- hgraph2graph-master/hgraph/__init__.py
+++ hgraph2graph-master/hgraph/__init__.py
@@ -3,4 +3,4 @@ from hgraph.encoder import HierMPNEncoder
 from hgraph.decoder import HierMPNDecoder
 from hgraph.vocab import Vocab, PairVocab, common_atom_vocab
 from hgraph.hgnn import HierVAE, HierVGNN, HierCondVGNN
-from hgraph.dataset import MoleculeDataset, MolPairDataset, DataFolder, MolEnumRootDataset
+from hgraph.dataset import MoleculeDataset, MolPairDataset, DataFolder, MolEnumRootDataset, DataFile
