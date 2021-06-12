89cf353195433575d9be7c76f443435648fec01c
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-06-12 23:22:51 +0200

    New code to consume less RAM

--- MoleculeMO-master/LSTM.py
+++ MoleculeMO-master/LSTM.py
@@ -7,7 +7,7 @@ from Modules.config import *
 from Modules.utils import time_elapsed, input_tensors, input_tensors_new
 import re
 from math import floor
-
+from Modules.utils import load_data
 
 # Define model
 class Model(nn.Module):
@@ -47,53 +47,66 @@ class Model(nn.Module):
 
 
 # Define training
-def train_epoch(model, data, int_data, optimizer, criterion, model_name='network.pth', output_file='training.txt',
+def train_epoch(model, data_list, int_data_list, optimizer, criterion, model_name='network.pth', output_file='training.txt',
                 count=0, t_final=0):
-    file = open(os.path.join(model_folder, output_file), "a")
-    # Set start time
-    start_time = time.time() - t_final
-    # Iterate set of seq_length characters
-    for i in range(int(floor(np.shape(data)[0] / batch_size * seq_length))):
-        # Initialize hidden and cell states
-        hidden = model.init_states(num_layers, hidden_size)
-        # Run on GPU if available
-        if cuda:
-            hidden = (hidden[0].cuda(), hidden[1].cuda())
-        # Set initial gradients
-        model.zero_grad()
-        # Set initial loss
-        loss = 0
-        # Get input and target
-        input_data, target_data = input_tensors_new(i, batch_size, seq_length, data, int_data)
-        input_data = input_data.float()
-        target_data = target_data.long()
-        # Run on GPU if available
-        if cuda:
-            input_data = input_data.cuda()
-            target_data = target_data.cuda()
-        # Run model, calculate loss
-        output, hidden = model(input_data, hidden)
-        loss += criterion(output.squeeze(), target_data.squeeze())
-        # Back propagate loss
-        loss.backward()
-        # Clip gradients
-        nn.utils.clip_grad_norm_(model.parameters(), 3.0)
-        # Optimize
-        optimizer.step()
-        # Update list of losses
-        losses.append(loss.data.item())
-        # Intermediary saves
-        if i % 10 == 0:
-            torch.save(model.state_dict(), os.path.join(model_folder, model_name))
-        # Print training info
-        hours, minutes, seconds = time_elapsed(start_time)
-        output_string = f"Loss: {loss.data.item() / seq_length:0.6f} | Delta: {losses[-1] - losses[-2]:+0.4f} | " + \
-                        f"Iteration: {i + 1 + count:06d} | Time elapsed: {hours:03d} h {minutes:02d} m {seconds:02d} s"
-        print(output_string)
-        file.write(output_string + '\n')
-        file.flush()
+
+    for data_filename, int_filename in zip(data_list, int_data_list):
+        data, int_data = load_data(data_filename, int_filename)
+        file = open(os.path.join(model_folder, output_file), "a")
+        # Set start time
+        start_time = time.time() - t_final
+        print(f'Each epoch has {int(floor(np.shape(data)[0] / (batch_size * seq_length)))} batches, aka iterations.')
+        # Iterate set of seq_length characters
+        for i in range(int(floor(np.shape(data)[0] / (batch_size * seq_length)))):
+            # Initialize hidden and cell states
+            hidden = model.init_states(num_layers, hidden_size)
+            # Run on GPU if available
+            if cuda:
+                hidden = (hidden[0].cuda(), hidden[1].cuda())
+            # Set initial gradients
+            model.zero_grad()
+            # Set initial loss
+            loss = 0
+            # Get input and target
+            input_data, target_data = input_tensors_new(i, batch_size, seq_length, data, int_data)
+            input_data = input_data.float()
+            target_data = target_data.long()
+            # Run on GPU if available
+            if cuda:
+                input_data = input_data.cuda()
+                target_data = target_data.cuda()
+            # Run model, calculate loss
+            output, hidden = model(input_data, hidden)
+            loss += criterion(output.squeeze(), target_data.squeeze())
+            # Back propagate loss
+            loss.backward()
+            # Clip gradients
+            nn.utils.clip_grad_norm_(model.parameters(), 3.0)
+            # Optimize
+            optimizer.step()
+            # Update list of losses
+            losses.append(loss.data.item())
+            count += 1
+            # Intermediary saves
+            if i % 10 == 0:
+                torch.save(model.state_dict(), os.path.join(model_folder, model_name))
+                # Print training info
+                hours, minutes, seconds = time_elapsed(start_time)
+                output_string = f"Loss: {loss.data.item() / seq_length:0.6f} | Delta: {losses[-1] - losses[-2]:+0.4f} | " + \
+                                f"Iteration: {count:06d} | Time elapsed: {hours:03d} h {minutes:02d} m {seconds:02d} s"
+                print(output_string)
+                file.write(output_string + '\n')
+                file.flush()
     torch.save(model.state_dict(), os.path.join(model_folder, model_name))
+    # Print training info
+    hours, minutes, seconds = time_elapsed(start_time)
+    output_string = f"Loss: {loss.data.item() / seq_length:0.6f} | Delta: {losses[-1] - losses[-2]:+0.4f} | " + \
+                    f"Iteration: {count:06d} | Time elapsed: {hours:03d} h {minutes:02d} m {seconds:02d} s"
+    print(output_string)
+    file.write(output_string + '\n')
+    file.flush()
     file.close()
+    return count
 
 
 def train(model, data, int_data, epochs, model_name, output_file='training.txt', new_session=False):
@@ -119,7 +132,7 @@ def train(model, data, int_data, epochs, model_name, output_file='training.txt',
     print("Total number of parameters in network: " + str(total_params))
     for i in range(epochs):
         # Train
-        train_epoch(model, data, int_data, optimizer, criterion, model_name=model_name, output_file=output_file,
+        count = train_epoch(model, data, int_data, optimizer, criterion, model_name=model_name, output_file=output_file,
                     count=count, t_final=t_final)
 
 
